8b51572cd74139146a4cb238c908fef43067ed0f
hdillon <dillonhoraciodavid@gmail.com>
2016-09-03 17:29:16 -0300

    Modificaciones tabs + acerca de..

--- Trivia_Ionic/www/js/controllers.js
+++ Trivia_Ionic/www/js/controllers.js
@@ -1,8 +1,8 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('LoginCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('JugarCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -21,8 +21,6 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AcercadeCtrl', function($scope) {
+  $scope.miFoto = 'img/perfil.png';
 });
